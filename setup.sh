3d8b9c448ed0e17249512a0bb1ccd0d47d5b74d4
fluffy <fluffy@beesbuzz.biz>
2018-04-18 22:34:53 -0700

    Another attempt at deploying to dreamhost

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@ if ! which pipenv > /dev/null ; then
     exit 1
 fi
 
-if [ ! -f config.py ] ; then
-    echo "Please copy config.py.dist to config.py and edit the settings accordingly (if appropriate)"
-    exit 1
-fi
-
 echo "Configuring environment..."
 
 pipenv install
