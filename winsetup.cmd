679317b35e027574abbd34a9430e570ce90d11f4
fluffy <fluffy@beesbuzz.biz>
2019-01-04 11:06:40 -0800

    Better first setup instructions

--- winsetup.cmd
+++ winsetup.cmd
@@ -4,6 +4,7 @@ TITLE Publ Setup
 pipenv --venv
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Running first-time install...
+    pip -q install pipenv
     pipenv --three install
 ) ELSE (
     ECHO Refreshing packages...
