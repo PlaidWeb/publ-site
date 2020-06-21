eab894009f815ec8a0a0c0034aedbc5925e73e3f
fluffy <fluffy@beesbuzz.biz>
2020-06-20 23:13:58 -0700

    Update pushl deployment step

--- deploy.sh
+++ deploy.sh
@@ -33,4 +33,4 @@ while [ $count -lt 5 ] && [ ! -S $HOME/.vhosts/publ.beesbuzz.biz ] ; do
     sleep $count
 done
 
-pipenv run pushl -rvvc cache http://publ.beesbuzz.biz/feed https://publ.beesbuzz.biz/feed
+pipenv run pushl -rvvc $HOME/var/pushl http://publ.beesbuzz.biz/feed https://publ.beesbuzz.biz/feed
